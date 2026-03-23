167d3d541946ce0dfee077a6e470c4edc1ea35d9
parcel <genericpb@gmail.com>
2026-03-23 11:08:08 -0500

    first

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use leptos::prelude::*;
+
+fn main() {
+    leptos::mount::mount_to_body(|| {
+        view! {
+            <div>
+            <p> "Helloe, world!"</p>
+            <p> "afdjhl, world!"</p>
+                </div>
+        }
+    })
+}
