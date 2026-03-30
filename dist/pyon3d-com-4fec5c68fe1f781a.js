8ef9203932df4bad7579d13cbf9a7c546dffa21d
parcel <genericpb@gmail.com>
2026-03-30 08:43:32 -0500

    Stuff

--- dist/pyon3d-com-4fec5c68fe1f781a.js
+++ dist/pyon3d-com-4fec5c68fe1f781a.js
@@ -328,7 +328,7 @@ function __wbg_get_imports() {
             return isLikeNone(ret) ? 0 : addToExternrefTable0(ret);
         },
         __wbindgen_cast_0000000000000001: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { dtor_idx: 34, function: Function { arguments: [Externref], shim_idx: 35, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
+            // Cast intrinsic for `Closure(Closure { dtor_idx: 33, function: Function { arguments: [Externref], shim_idx: 34, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
             const ret = makeMutClosure(arg0, arg1, wasm.wasm_bindgen__closure__destroy__h1db240e33170d0b9, wasm_bindgen__convert__closures_____invoke__h1ab0969185925d7a);
             return ret;
         },
--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ fn Button<'a>(text: &'a str) -> impl IntoView {
 fn Feature<'a>(title: &'a str, children: Children) -> impl IntoView {
     view! {
         <div class="flex flex-col w-full h-full bg-[#b0b0b0]">
-            <div class="font-semibold px-1">{title}</div>
+            <div class="font-semibold px-1 whitespace-nowrap">{title}</div>
             <div class="bg-[#c0c0c0] h-full text-sm px-1 pb-2">{children()}</div>
         </div>
     }
@@ -26,49 +26,66 @@ fn Feature<'a>(title: &'a str, children: Children) -> impl IntoView {
 fn main() {
     console_error_panic_hook::set_once();
 
+    // Social links order
+    // Patreon, bsky
+
+    /*
+    <div class="flex flex-row justify-center w-full">
+        <div class="flex flex-row border-2 divide-x-2 ">
+            <Button text="try/buy" />
+            <Button text="subscribe" />
+        </div>
+    </div>
+    */
+
     leptos::mount::mount_to_body(|| {
         view! {
             <div class="w-screen h-screen flex flex-col items-center font-sans">
-                <div class="bg-[#e0e0e0] max-w-[800px] min-h-full flex flex-col p-1 gap-4">
-                    <div class="flex flex-row border-2 divide-x-2">
-                        <div class="px-1 py-0.5">status: prealpha</div>
-                        <div class="grow min-w-0" />
-                        <Button text="info" />
-                        <Button text="manual" />
-                        <Button text="donate" />
+                <div class="bg-[#e0e0e0] max-w-[800px] min-h-full flex flex-col gap-3">
+                    <div class="flex flex-row gap-2 px-1 pt-1">
+                        <div class="flex flex-row border-2 divide-x-2 grow min-w-0">
+                            <div class="px-1 py-0.5">"status: prealpha"</div>
+                            <div class="grow min-w-0" />
+                            <Button text="info" />
+                            <Button text="manual" />
+                            <Button text="try/buy" />
+                        </div>
+                        <Button text="P" />
+                        <Button text="B" />
                     </div>
                     <div class="flex flex-row w-full justify-center gap-6 items-center">
                         <div class="text-2xl">pyon3d</div>
                         <div>a comfy 3d editor</div>
                     </div>
                     <div class="flex flex-row w-full aspect-video bg-black text-white relative">
-                        <div class="absolute top-1 left-1 bg-yellow-500 text-black px-1 py-0.5">
-                            NOTE: Not a real screenshot of pyon3d a comfy 3d editor
+                        <div class="absolute bg-yellow-500 text-black px-1 py-0.5">
+                            "NOTE: Not a real screenshot of pyon3d"
                         </div>
-                        <img class="object-fill w-full" src="public/blender-screenshot.png" />
+                        <img class="object-fill w-full" src="blender-screenshot.png" />
                     </div>
-                    <div class="grid grid-cols-3 px-2 gap-1">
-                        <Feature title="Quick to use">{"\"Sculpting for low-poly\""}</Feature>
-                        <Feature title="For gamedevs">
-                            Preview shaders live. Automatically reload assets as you work on them. Scripting.
-                            Extensible data model.
-                        </Feature>
-                        <Feature title="Easy to learn">
-                            Clean UI. Straightforward user experience
-                        </Feature>
-                    </div>
-                    <div class="flex flex-row gap-4">
-                        planned: multilanguage support, highpoly, iOS/android, plugin system
-                    </div>
-                    <div class="flex flex-row justify-center w-full">
-                        <div class="flex flex-row border-2 divide-x-2 ">
-                            <Button text="try/buy" />
-                            <Button text="subscribe" />
+                    <div class="w-full px-3 flex flex-col gap-2">
+                        <div class="grid grid-cols-3 gap-1">
+                            <Feature title="Quick to use">
+                                "\"Sculpting for low-poly\". Workflow inspried by 2D art tools."
+                            </Feature>
+                            <Feature title="For gamedevs">
+                                "Preview shaders live. Automatically reload assets as you work on them. Game loop and interaction
+                                scripting. Extensible data model."
+                            </Feature>
+                            <Feature title="Easy to learn">
+                                "Clean UI. Straightforward user experience. Good documentation."
+                            </Feature>
+                        </div>
+                        <div class="w-full flex flex-row items-center bg-[#c0c0c0]">
+                            <div class="bg-[#b0b0b0] px-1 font-semibold">Planned</div>
+                            <div class="grow min-w-0 px-1 text-sm">
+                                "multilanguage support, high-poly modelling tools, iOS/android/tablet, plugin system, themable UI"
+                            </div>
                         </div>
                     </div>
                     <div class="grow min-h-0" />
-                    <div class="w-full text-center">
-                        made with love BY HUMANS in Chicago {" 🌭"}
+                    <div class="w-full text-center pb-1">
+                        made with love by humans in Chicago {" 🌭 "}(c) pyondotmoe 2026
                     </div>
                 </div>
             </div>
